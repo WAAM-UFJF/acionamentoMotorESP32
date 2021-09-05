b25afedb50374027dbf77eb543dd9d7ef4e429ee
ThiiD <thiago.saber@engenharia.ufjf.br>
2021-09-05 13:22:49 -0300

    Testes iniciais e pinagem
    Testes iniciais e pinagem

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <Arduino.h>
+int led  = 2;
+
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  digitalWrite(led, HIGH);
+  Serial.println("Led Ligado");
+  delay(1000);
+  digitalWrite(led, LOW);
+  Serial.println("Led desligado.");
+  delay(1000);
+
 }
