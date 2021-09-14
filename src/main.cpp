66f1be1dec09cbafe03ba15de017a718f36d83bd
ThiiD <thiago.saber@engenharia.ufjf.br>
2021-09-14 18:43:10 -0300

    Acionamento em Malha Aberta

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,47 @@ const int sentidoMotor2 = 0;  // Porta para definir o sentido de rotação 2.
 
 const int freq = 5000;        // Define a frequencia a ser utilizada
 const int ledChannel = 0;
-const int resolution = 8;     // Define a resolução que será utilizada no PWM.
+int resolution = 8;           // Define a resolução que será utilizada no PWM.
 
 
 void setup() {
   Serial.begin(115200);
+  pinMode(sentidoMotor1, OUTPUT);
+  pinMode(sentidoMotor2, OUTPUT);
+  pinMode(led, OUTPUT);
+  digitalWrite(sentidoMotor1, LOW);
+  digitalWrite(sentidoMotor2, HIGH);
+
+  // Atribui o canal ao GPIO que será controlado
+  ledcAttachPin(led, ledChannel);
 
   // Configura o LED PWM
   ledcSetup(ledChannel, freq, resolution);
 
-  // Atribui o canal ao GPIO que será controlado
-  ledcAttachPin(led, ledChannel);
+  
+
+  // Define como output os pinos que definem o sentido de rotação do motor
+  
 }
 
 void loop() {
 
    // Aumenta a velocidade de rotação do motor
-  for(int dutyCycle = 0; dutyCycle <= (2^resolution - 1); dutyCycle++){   
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
     // Aumenta a velocidade de rotação do motor através do aumento do dutycycle do PWM
     ledcWrite(ledChannel, dutyCycle);
-    delay(15);
+    Serial.print("DutyCycle: ");
+    Serial.println(dutyCycle);
+    delay(100);
   }
 
   // Diminui a valocidade de rotação do motor
-  for(int dutyCycle = (2^resolution - 1); dutyCycle >= 0; dutyCycle--){
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
     // Diminui a velocidade de rotação do motor através do decremento do dutycycle do PWM
-    ledcWrite(ledChannel, dutyCycle);   
-    delay(15);
+    ledcWrite(ledChannel, dutyCycle);
+    Serial.print("DutyCycle: ");
+    Serial.println(dutyCycle);
+    delay(100);
   }
 
 }
